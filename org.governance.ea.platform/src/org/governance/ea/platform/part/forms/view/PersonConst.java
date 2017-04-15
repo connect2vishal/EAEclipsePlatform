1a7967ef223d621980e6935d09abcb79c4c90cc3
Vishal Kumar <connect2vishal@gmail.com>
2017-04-16 03:29:19 +0400

    need to get input and output attributes... that we wait till tomorrow
    need to get input and output attributes... that we wait till tomorrow

--- org.governance.ea.platform/src/org/governance/ea/platform/part/forms/view/PersonConst.java
+++
@@ -1,8 +0,0 @@
-package org.governance.ea.platform.part.forms.view;
-
-public interface PersonConst {
-
-	int COLUMN_FIRST_NAME = 0;
-	int COLUMN_LAST_NAME = 1;
-
-}
--- org.governance.ea.platform/src/org/governance/ea/platform/util/GridViewUtil.java
+++ org.governance.ea.platform/src/org/governance/ea/platform/util/GridViewUtil.java
@@ -15,9 +15,17 @@ import javax.swing.table.DefaultTableModel;
 import org.apache.commons.io.FileUtils;
 import org.governance.ea.platform.part.forms.view.WSDLServiceModel;
 
+import com.predic8.schema.Schema;
+import com.predic8.wsdl.AbstractSOAPBinding;
+import com.predic8.wsdl.Binding;
+import com.predic8.wsdl.BindingOperation;
 import com.predic8.wsdl.Definitions;
+import com.predic8.wsdl.Fault;
+import com.predic8.wsdl.Message;
 import com.predic8.wsdl.Operation;
+import com.predic8.wsdl.Part;
 import com.predic8.wsdl.Port;
+import com.predic8.wsdl.PortType;
 import com.predic8.wsdl.Service;
 import com.predic8.wsdl.WSDLParser;
 
@@ -26,6 +34,101 @@ import com.predic8.wsdl.WSDLParser;
  *
  */
 public class GridViewUtil {
+
+	private static void printWSDL(Definitions tempDefs){
+		
+		Definitions defs = tempDefs;
+	 
+	    out("-------------- WSDL Details --------------");
+	    out("TargenNamespace: \t" + defs.getTargetNamespace());
+	    //out("Style: \t\t\t" + defs.getStyle());
+	    if (defs.getDocumentation() != null) {
+	        out("Documentation: \t\t" + defs.getDocumentation());
+	    }
+	    out("\n");
+	
+	    /* For detailed schema information see the FullSchemaParser.java sample.*/
+	    out("Schemas: ");
+	    for (Schema schema : defs.getSchemas()) {
+	        out("  TargetNamespace: \t" + schema.getTargetNamespace());
+	    }
+	    out("\n");
+	     
+	    out("Messages: ");
+	    for (Message msg : defs.getMessages()) {
+	        out("  Message Name: " + msg.getName());
+	        out("  Message Parts: ");
+	        for (Part part : msg.getParts()) {
+	            out("    Part Name: " + part.getName());
+	            out("    Part Element: " + ((part.getElement() != null) ? part.getElement() : "not available!"));
+	            out("    Part Type: " + ((part.getType() != null) ? part.getType() : "not available!" ));
+	            out("");
+	        }
+	    }
+	    out("");
+	
+	    out("PortTypes: ");
+	    for (PortType pt : defs.getPortTypes()) {
+	        out("  PortType Name: " + pt.getName());
+	        out("  PortType Operations: ");
+	        for (Operation op : pt.getOperations()) {
+	            out("    Operation Name: " + op.getName());
+	            out("    Operation Input Name: "
+	                + ((op.getInput().getName() != null) ? op.getInput().getName() : "not available!"));
+	            out("    Operation Input Message: "
+	                + op.getInput().getMessage().getQname());
+	            out("    Operation Output Name: "
+	                + ((op.getOutput().getName() != null) ? op.getOutput().getName() : "not available!"));
+	            out("    Operation Output Message: "
+	                + op.getOutput().getMessage().getQname());
+	            out("    Operation Faults: ");
+	            if (op.getFaults().size() > 0) {
+	                for (Fault fault : op.getFaults()) {
+	                    out("      Fault Name: " + fault.getName());
+	                    out("      Fault Message: " + fault.getMessage().getQname());
+	                }
+	            } else out("      There are no faults available!");
+	             
+	        }
+	        out("");
+	    }
+	    out("");
+	
+	    out("Bindings: ");
+	    for (Binding bnd : defs.getBindings()) {
+	        out("  Binding Name: " + bnd.getName());
+	        out("  Binding Type: " + bnd.getPortType().getName());
+	        out("  Binding Protocol: " + bnd.getBinding().getProtocol());
+	        if(bnd.getBinding() instanceof AbstractSOAPBinding) out("  Style: " + (((AbstractSOAPBinding)bnd.getBinding()).getStyle()));
+	        out("  Binding Operations: ");
+	        for (BindingOperation bop : bnd.getOperations()) {
+	            out("    Operation Name: " + bop.getName());
+	            if(bnd.getBinding() instanceof AbstractSOAPBinding) {
+	                out("    Operation SoapAction: " + bop.getOperation().getSoapAction());
+	                out("    SOAP Body Use: " + bop.getInput().getBindingElements().get(0).getUse());
+	            }
+	        }
+	        out("");
+	    }
+	    out("");
+	
+	    out("Services: ");
+	    for (Service service : defs.getServices()) {
+	        out("  Service Name: " + service.getName());
+	        out("  Service Potrs: ");
+	        for (Port port : service.getPorts()) {
+	            out("    Port Name: " + port.getName());
+	            out("    Port Binding: " + port.getBinding().getName());
+	            out("    Port Address Location: " + port.getAddress().getLocation()
+	                + "\n");
+	        }
+	    }
+	    out("");
+	}
+	
+	private static void out(String str) {
+	    System.out.println(str);
+	}
 
 	/**
 	 * @return
@@ -71,6 +174,7 @@ public class GridViewUtil {
 			} catch (Exception e) {
 				folderName = "--N/A--";
 			} 
+			//printWSDL(defs);
 			reqWSDLServiceModelList.addAll(getWSDLServiceModel(name, folderName, defs));
 		}
 
@@ -117,7 +221,9 @@ public class GridViewUtil {
 						wsdlServiceModel.setServiceName(serv.getName());
 						wsdlServiceModel.setPortBinding(port.getName());
 						wsdlServiceModel.setOperationName(ops.getName());
-						wsdlServiceModel.setRequestName(ops.getInput().getMessage().getName());//getName());
+						//wsdlServiceModel.setRequestName(ops.getInput().getMessage().getName());//.getParts().get(0).getElement().getName());//.getName());//getName());
+						
+						wsdlServiceModel.setRequestName(ops.getInput().getMessage().getParts().get(0).getElement().getName());
 						wsdlServiceModel.setResponseName(ops.getOutput().getMessage().getName());
 						
 						wsdlServiceModelList.add(wsdlServiceModel);
